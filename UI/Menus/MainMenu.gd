f413c66adc59d46b2992e32d19e8e60fa9ee2321
HarmlessValve <Altagemilang@protonmail.com>
2026-08-19 02:11:12 +0700

    modified: restructure menus node and fixing fullscreen issues

--- UI/Menus/MainMenu.gd
+++ UI/Menus/MainMenu.gd
@@ -1,14 +1,18 @@
-extends Control
+extends Node2D
 
 func _on_start_button_pressed() -> void:
+	var canvas_layer = $CanvasLayer
 	var fade_rect = ColorRect.new()
 	fade_rect.color = Color(0, 0, 0, 0)
 	fade_rect.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
-	add_child(fade_rect)
-	
+	canvas_layer.add_child(fade_rect)
+
 	var tween = create_tween()
 	tween.tween_property(fade_rect, "color", Color(0, 0, 0, 1), 0.3)
 	tween.tween_callback(func(): get_tree().change_scene_to_file("res://scenes/map.tscn"))
 
-func _on_exit_button_pressed() -> void:
+func _on_settings_button_pressed() -> void:
+	pass # TODO: Buka scene Settings
+
+func _on_quit_button_pressed() -> void:
 	get_tree().quit()
